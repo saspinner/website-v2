f896d7f2a39f3f983b2cabab363b4fd1f55d83c5
Sam Spinner <samspinner4@gmail.com>
2025-12-14 04:40:37 +0000

    pause for here today, you are good to move on from dry runs, logic is working as expected

--- reset.sh
+++ reset.sh
@@ -2,3 +2,4 @@
 sudo systemctl stop blog_inotifywatch
 sudo systemctl daemon-reload
 sudo systemctl start blog_inotifywatch
+sudo journalctl -f -u blog_inotifywatch.service
--- watch_for_updates.sh
+++ watch_for_updates.sh
@@ -3,17 +3,17 @@
 obsidian_dir="/home/sam/syncthing/obsidian/blog/"
 website_dir="/home/sam/projects/website-v2/src/pages/posts/"
 website_template="../../layouts/MarkdownLayout.astro"
+GIT="git --git-dir=/home/sam/projects/website-v2/.git --work-tree=/home/sam/projects/website-v2"
 sleep_time=1
-
-add_metadata() {
-	awk '--- { print; print "layout: $website_template"; }' "$1"
-}
 
 safe_git_add_commit() {
 	git pull
 	git add "$1"
 	git commit -m "auto-adding file $1 via watch_for_updates script"
 }
+
+# systemd are run from /, need to change to git directory
+cd /home/sam/projects/website-v2 || exit
 
 inotifywait -r -m /home/sam/syncthing/obsidian/blog -e close --format "%f%0" |
 while IFS= read -r -d '' file; do 
@@ -32,32 +32,26 @@ while IFS= read -r -d '' file; do
 
 	# TODO: remember to add metadata
 
-	# if new file, git diff empty -> git add, commit
-	diff=$(echo git diff "$website_filepath")
-	echo "result of git diff: $diff"
-	if [-z "$diff"]; then
+	# if it's a new file, add a commit
+	if [ -z "$($GIT ls-files "$website_filepath")" ]; then
 		#safe_git_add_commit "$website_filepath"
-		echo "would call safe_git_add_commit $website_filepath"
+		echo "would call safe_git_add_commit $website_filepath because no git diff found"
 		
-	fi
+	else
 
-	if grep -q '^live: true' "$website_filepath"; then
-		# push here
-		echo "would push changes made to $website_filepath because live is set to true"
+		if grep -q '^live: true' "$website_filepath"; then
+			# push here
+			echo "would push changes made to $website_filepath because live is set to true"
 
-	fi
+		fi
  
-	if $(echo "$diff" | grep q '+live: false'); then
-		echo "would push changes because $website_filepath had live set to false"
+		
+		diff=$($GIT diff "$website_filepath")
+		if echo "$diff" | grep -q '+live: false'; then
+			echo "would push changes because $website_filepath had live set to false"
+		fi
+
 	fi
-
-
-
-	# situations where we want to push
-	# 1. wasn't alive, is now (git diff has +live: true)
-	# 2. was alive, is no longer (git diff has +live: false)
-	# 3 is alive, any changes made
-
 
 done
 
